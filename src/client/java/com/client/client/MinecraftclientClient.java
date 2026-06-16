2c76a76c094de694cfe15e3da9a84887e073b5cf
YourName <you@example.com>
2026-06-16 13:00:16 -0500

    Initial commit

---
+++ src/client/java/com/client/client/MinecraftclientClient.java
@@ -0,0 +1,10 @@
+package com.client.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinecraftclientClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
